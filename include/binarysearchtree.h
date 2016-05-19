0e2e886b7009ba2c81013add76cede2e39be6825
skm2696 <skm03092014@gmail.com>
2016-05-19 23:18:39 +0300

    Update binarysearchtree.h

--- include/binarysearchtree.h
+++ include/binarysearchtree.h
@@ -27,9 +27,9 @@ class File_Not_Open : public Exceptions {
 public:
 	File_Not_Open();
 };
-class Empty : public Exceptions {
+class Empty_tree : public Exceptions {
 public:
-	Empty();
+	Empty_tree();
 };
 class Exist : public Exceptions {
 public:
@@ -48,7 +48,7 @@ Exceptions::Exceptions(char* _err) : err(_err) {}
 char* Exceptions::what() { return err; }
 Exist::Exist() : Exceptions("ERROR: It's already exist!") {}
 File_Not_Open::File_Not_Open() : Exceptions("ERROR: file is not open!") {}
-Empty::Empty() : Exceptions("ERROR: The Binary Tree is empty!") {}
+Empty_tree::Empty_tree() : Exceptions("ERROR: The Binary Tree is empty!") {}
 Deleted::Deleted() : Exceptions("ERROR: It was deleted before") {}
 Error_stream::Error_stream() : Exceptions("ERROR: Stream error") {}
 template <class T>
@@ -63,48 +63,48 @@ public:
 	bool operator == (const BinarySearchTree<T> & tree);
 	auto begin() const->iterator;
 	auto end() const->iterator;
-	bool Insert(T x);// Добавление элемента 
-	bool Search(T x);// Поиск элемента 
-	bool del(T x); //удаление узла дерева 
+	bool add(T x);
+	bool Search(T x);
+	bool del(T x);  
 	size_t size();
 	friend ostream & operator<< <>(ostream &out, BinarySearchTree<T> &tree);
 	friend ofstream & operator<< <>(ofstream &fout, BinarySearchTree<T> &tree);
 	friend ifstream & operator>> <>(ifstream &fin, BinarySearchTree<T> &tree);
 	friend istream & operator >> <>(istream & in, BinarySearchTree<T> &tree);
-	class Root;
-	Root* root; //корень дерева 
+	class ELEM;
+	ELEM* root; 
 
-	static auto  fillListOfNodes(list<const Root*> & listOfNodes, const Root* root)->void;
+	static auto  fillListOfNodes(list<const ELEM*> & listOfNodes, const ELEM* root)->void;
 	class NodeIterator {
 	public:
 		NodeIterator() = default;
-		NodeIterator(const Root* root);
+		NodeIterator(const ELEM* root);
 		auto operator == (const NodeIterator & _iterator) -> bool;
 		auto operator != (const NodeIterator & _iterator) -> bool;
 		auto operator ++()->NodeIterator;
 		auto operator *() const -> const T &;
-		list<const Root*> history_;
+		list<const ELEM*> history_;
 		
 	};
 };
 
 template <class T>
-class BinarySearchTree<T>::Root {
+class BinarySearchTree<T>::ELEM {
 public:
-	Root(T x);
-	void destroy(Root* root);
-	void Insert(T x);
+	ELEM(T x);
+	void destroy(ELEM* root);
+	void add(T x);
 	bool Search(T x);
 	size_t size_;
 	size_t sizeRoot();
 	bool print_console();
 	bool print_file(ofstream &fout);
 	void del(T el);
-	T find_min(Root* el);
+	T find_min(ELEM* el);
 	T get_data() { return D; };
 	T D;
-	Root *l;
-	Root *r;
+	ELEM *l;
+	ELEM *r;
 private:
 	
 };
