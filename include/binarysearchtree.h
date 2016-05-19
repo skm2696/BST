3a8797a602b28e9b32703a31409d941a86ad3d8b
skm2696 <skm03092014@gmail.com>
2016-05-19 20:27:40 +0300

    Update binarysearchtree.h

--- include/binarysearchtree.h
+++ include/binarysearchtree.h
@@ -1,93 +1,110 @@
-#ifndef binarysearchtree_h
-#define binarysearchtree_h
-#pragma once
-#include <iostream>
-#include <fstream>
+#include <iostream> 
+#include <fstream> 
+#include <list> 
 using namespace std;
 
 template <class T>
-class BinarySearchTree;
+class Tree;
+template <class T>
+ostream & operator<<(ostream & out, Tree<T> &tree);
 
 template <class T>
-ostream & operator<<(ostream & out, BinarySearchTree<T> &tree);
+ofstream & operator<<(ofstream & fout, Tree<T> &tree);
 
 template <class T>
-ofstream & operator<<(ofstream & fout, BinarySearchTree<T> &tree);
+ifstream & operator >> (ifstream & fin, Tree<T> &tree);
 
 template <class T>
-ifstream & operator >> (ifstream & fin, BinarySearchTree<T> &tree);
-class Exception
-{
+istream & operator >> (istream & in, Tree<T> &tree);
+//Класс исключений 
+class Exceptions {
 	char* err;
 public:
-	Exception(char* _err);
+	Exceptions(char* _err);
 	char* what();
 };
-
-class Match_elem : public Exception
-{
-public:
-	Match_elem();
-};
-
-class File_Not_Open : public Exception
-{
+class File_Not_Open : public Exceptions {
 public:
 	File_Not_Open();
 };
-
-class Empty_tree : public Exception
-{
+class Empty : public Exceptions {
 public:
-	Empty_tree();
+	Empty();
+};
+class Exist : public Exceptions {
+public:
+	Exist();
+};
+class Deleted : public Exceptions {
+public:
+	Deleted();
+};
+class Error_stream : public Exceptions {
+public:
+	Error_stream();
 };
 
-class Element_not_found : public Exception{  
+Exceptions::Exceptions(char* _err) : err(_err) {}
+char* Exceptions::what() { return err; }
+Exist::Exist() : Exceptions("ERROR: It's already exist!") {}
+File_Not_Open::File_Not_Open() : Exceptions("ERROR: file is not open!") {}
+Empty::Empty() : Exceptions("ERROR: The Binary Tree is empty!") {}
+Deleted::Deleted() : Exceptions("ERROR: It was deleted before") {}
+Error_stream::Error_stream() : Exceptions("ERROR: Stream error") {}
+template <class T>
+class Tree {
 public:
-	Element_not_found();
-};
+	class NodeIterator;
+	using iterator = NodeIterator;
 
-class Tree_Was_Deleted : public Exception{  
-public:
-	Tree_Was_Deleted();
+	Tree() :root(nullptr) {};
+	Tree(const initializer_list<T> & ilist);
+	~Tree();
+	bool operator == (const Tree<T> & tree);
+	auto begin() const->iterator;
+	auto end() const->iterator;
+	bool Insert(T x);// Добавление элемента 
+	bool Search(T x);// Поиск элемента 
+	bool del(T x); //удаление узла дерева 
+	size_t size();
+	friend ostream & operator<< <>(ostream &out, Tree<T> &tree);
+	friend ofstream & operator<< <>(ofstream &fout, Tree<T> &tree);
+	friend ifstream & operator>> <>(ifstream &fin, Tree<T> &tree);
+	friend istream & operator >> <>(istream & in, Tree<T> &tree);
+	class Root;
+	Root* root; //корень дерева 
+
+	static auto  fillListOfNodes(list<const Root*> & listOfNodes, const Root* root)->void;
+	class NodeIterator {
+	public:
+		NodeIterator() = default;
+		NodeIterator(const Root* root);
+		auto operator == (const NodeIterator & _iterator) -> bool;
+		auto operator != (const NodeIterator & _iterator) -> bool;
+		auto operator ++()->NodeIterator;
+		auto operator *() const -> const T &;
+		list<const Root*> history_;
+		
+	};
 };
 
 template <class T>
-class BinarySearchTree{
+class Tree<T>::Root {
 public:
-	BinarySearchTree();
-	BinarySearchTree(initializer_list<T> L);
-	~BinarySearchTree();
-	bool add(T x);
-	bool search(T x);
-	bool del(T x);
-	void new_der();
-	friend ostream & operator<< <>(ostream &out, BinarySearchTree<T> &tree);
-	friend ofstream & operator<< <>(ofstream &fout, BinarySearchTree<T> &tree);		
-	friend ifstream & operator>> <>(ifstream &fin, BinarySearchTree<T> &tree);
-private:
-	class ELEM; 
-	ELEM* root;
-};
-
-template <class T>
-class BinarySearchTree<T>::ELEM{
-public:
-	ELEM(T x);
-	void add(T x);
-	bool search(T x);
+	Root(T x);
+	void destroy(Root* root);
+	void Insert(T x);
+	bool Search(T x);
+	size_t size_;
+	size_t sizeRoot();
 	bool print_console();
 	bool print_file(ofstream &fout);
-	void del(T x);
-	void do_free();
-	T min(ELEM* G);
-private:
+	void del(T el);
+	T find_min(Root* el);
+	T get_data() { return D; };
 	T D;
-	ELEM *l;
-	ELEM *r;
+	Root *l;
+	Root *r;
+private:
+	
 };
-
-
-
-
-#endif
